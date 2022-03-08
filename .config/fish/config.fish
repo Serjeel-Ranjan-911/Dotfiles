31d12e17e03133c0345de08d4eb334f80a45ac1e
Serjeel-Ranjan-911 <serjeelranjan@gmail.com>
2022-03-08 14:53:44 +0530

    basic dotfiles and icons

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+printf "\e[38;5;118m"
+figlet "    Serjeel"
+
+printf "\e[38;5;75m"
+motivate
+
+function clr
+    clear
+    printf "\e[38;5;118m"
+    figlet "    Serjeel"
+
+    printf "\e[38;5;75m"
+    motivate
+end
+
+funcsave clr 
+
+fish_add_path -a /usr/lib64/openjdk-11/bin
---
+++ .config/fish/functions/clr.fish
@@ -0,0 +1,8 @@
+function clr
+    clear
+    printf "\e[38;5;118m"
+    figlet "    Serjeel"
+
+    printf "\e[38;5;75m"
+    motivate
+end
